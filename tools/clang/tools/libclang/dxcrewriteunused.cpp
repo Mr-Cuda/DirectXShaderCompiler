21d5809609e732329b9361adfa966115c5bb64e4
Lukas Hermanns <lukas.hermanns@epicgames.com>
2020-11-12 15:10:10 -0500

    Enable Vulkan specific features in rewriter.

--- tools/clang/tools/libclang/dxcrewriteunused.cpp
+++ tools/clang/tools/libclang/dxcrewriteunused.cpp
@@ -548,6 +548,9 @@ void SetupCompilerCommon(CompilerInstance &compiler,
   compiler.getLangOpts().EnableFXCCompatMode = opts.EnableFXCCompatMode;
   compiler.getDiagnostics().setIgnoreAllWarnings(!opts.OutputWarnings);
   compiler.getCodeGenOpts().MainFileName = pMainFile;
+  // UE Change Begin: Enable Vulkan specific features in rewriter.
+  compiler.getLangOpts().SPIRV = opts.GenSPIRV;
+  // UE Change End: Enable Vulkan specific features in rewriter.
 
   PreprocessorOptions &PPOpts = compiler.getPreprocessorOpts();
   if (rewrite != nullptr) {
@@ -980,6 +983,9 @@ DoRewriteUnused(_In_ DxcLangExtensionsHelper *pHelper, _In_ LPCSTR pFileName,
   ASTHelper astHelper;
   hlsl::options::DxcOpts opts;
   opts.HLSLVersion = hlsl::LangStd::v2015;
+  // UE Change Begin: Enable Vulkan specific features in rewriter.
+  opts.GenSPIRV = true;
+  // UE Change End: Enable Vulkan specific features in rewriter.
 
   GenerateAST(pHelper, pFileName, pRemap, pDefines, defineCount, astHelper,
               opts, msfPtr, w);
@@ -1646,6 +1652,9 @@ public:
 
       hlsl::options::DxcOpts opts;
       opts.HLSLVersion = hlsl::LangStd::v2015;
+      // UE Change Begin: Enable Vulkan specific features in rewriter.
+      opts.GenSPIRV = true;
+      // UE Change End: Enable Vulkan specific features in rewriter.
 
       std::string errors;
       std::string rewrite;
@@ -1696,6 +1705,9 @@ public:
 
       hlsl::options::DxcOpts opts;
       opts.HLSLVersion = hlsl::LangStd::v2015;
+      // UE Change Begin: Enable Vulkan specific features in rewriter.
+      opts.GenSPIRV = true;
+      // UE Change End: Enable Vulkan specific features in rewriter.
 
       opts.RWOpt.SkipFunctionBody |=
           rewriteOption & RewriterOptionMask::SkipFunctionBody;
